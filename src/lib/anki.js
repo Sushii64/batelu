97420d4b4bb99cae7236ce3c38c2cff6889f9d15
mybearworld <130385691+mybearworld@users.noreply.github.com>
2026-06-28 10:23:44 +0200

    Use word as key for Anki tool

--- src/lib/anki.js
+++ src/lib/anki.js
@@ -1,5 +1,23 @@
 import { words } from "./words.js";
 
-export const ankiFormatWords = words
-  .map((word) => `${word.definition}\t${word.displayWord}`)
-  .join("\n");
+const getAnkiFormatWords = () => {
+  const wordDefinitionMapping = new Map();
+  words.forEach((word) => {
+    const existingDefinition = wordDefinitionMapping.get(word.displayWord);
+    if (existingDefinition) {
+      wordDefinitionMapping.set(
+        word.displayWord,
+        existingDefinition + " / " + word.definition,
+      );
+    } else {
+      wordDefinitionMapping.set(word.displayWord, word.definition);
+    }
+  });
+  return [
+    ...wordDefinitionMapping
+      .entries()
+      .map(([word, definition]) => `${word}\t${definition}`),
+  ].join("\n");
+};
+
+export const ankiFormatWords = getAnkiFormatWords();
